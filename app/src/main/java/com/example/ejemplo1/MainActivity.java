b9b426b3e812962b99245634b60337fe616ea6f9
RodZZer <104182519+RodZZer@users.noreply.github.com>
2022-05-05 16:20:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ejemplo1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ejemplo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1,et2;
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=(EditText)findViewById(R.id.txt_n1);
+        et2=(EditText)findViewById(R.id.txt_n2);
+        tv1=(TextView)findViewById(R.id.tv_r1);
+    }
+    public void Calcular(View view){
+        String valor1_String = et1.getText().toString();
+        int valor1_int = Integer.parseInt(valor1_String);
+
+        String valor2_String = et2.getText().toString();
+        int valor2_int = Integer.parseInt(valor2_String);
+
+        int resul = valor1_int+valor2_int;
+
+        tv1.setText(String.valueOf(resul));
+    }
+}
