1913b4b0a93df4133d2b253fd17b635cdc33c027
RodZZer <104182519+RodZZer@users.noreply.github.com>
2022-05-05 22:21:37 -0400

    Ejemplo1 Con suma/resta/multiplicación/división

--- app/src/main/java/com/example/ejemplo1/MainActivity.java
+++ app/src/main/java/com/example/ejemplo1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         et2=(EditText)findViewById(R.id.txt_n2);
         tv1=(TextView)findViewById(R.id.tv_r1);
     }
-    public void Calcular(View view){
+    public void Sumar(View view){
         String valor1_String = et1.getText().toString();
         int valor1_int = Integer.parseInt(valor1_String);
 
@@ -26,6 +26,39 @@ public class MainActivity extends AppCompatActivity {
         int valor2_int = Integer.parseInt(valor2_String);
 
         int resul = valor1_int+valor2_int;
+
+        tv1.setText(String.valueOf(resul));
+    }
+    public void Restar(View view){
+        String valor1_String = et1.getText().toString();
+        int valor1_int = Integer.parseInt(valor1_String);
+
+        String valor2_String = et2.getText().toString();
+        int valor2_int = Integer.parseInt(valor2_String);
+
+        int resul = valor1_int-valor2_int;
+
+        tv1.setText(String.valueOf(resul));
+    }
+    public void Multiplicar(View view){
+        String valor1_String = et1.getText().toString();
+        int valor1_int = Integer.parseInt(valor1_String);
+
+        String valor2_String = et2.getText().toString();
+        int valor2_int = Integer.parseInt(valor2_String);
+
+        int resul = valor1_int*valor2_int;
+
+        tv1.setText(String.valueOf(resul));
+    }
+    public void Dividir(View view){
+        String valor1_String = et1.getText().toString();
+        int valor1_int = Integer.parseInt(valor1_String);
+
+        String valor2_String = et2.getText().toString();
+        int valor2_int = Integer.parseInt(valor2_String);
+
+        int resul = valor1_int/valor2_int;
 
         tv1.setText(String.valueOf(resul));
     }
